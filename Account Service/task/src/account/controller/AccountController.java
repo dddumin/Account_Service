8796220fc9405838f4de9316190bd5bb276cc61a
dumin <dumin@protei.ru>
2022-03-26 00:53:14 +0300

    stage 2

--- Account Service/task/src/account/controller/AccountController.java
+++ Account Service/task/src/account/controller/AccountController.java
@@ -1,6 +1,6 @@
 package account.controller;
 
-import account.model.dto.AuthToken;
+import account.model.exception.UserExistException;
 import account.model.jdbc.Account;
 import account.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class AccountController {
     private AccountService accountService;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Autowired
     public void setAccountService(AccountService accountService) {
@@ -24,11 +33,25 @@ public class AccountController {
 
     @PostMapping(path = "/auth/signup")
     public ResponseEntity<?> create(@Valid @RequestBody Account account) {
-        return new ResponseEntity<>(accountService.create(account), HttpStatus.OK);
+        Account acc = null;
+        try {
+            account.setPassword(passwordEncoder.encode(account.getPassword()));
+            acc = accountService.create(account);
+        } catch (UserExistException e) {
+            return new ResponseEntity<Object>(
+                    Map.of("timestamp", LocalDateTime.now(),
+                            "status", 400,
+                            "error", "Bad Request",
+                            "message", "User exist!",
+                            "path", "/api/auth/signup"),
+                    HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(acc, HttpStatus.OK);
     }
 
     @GetMapping(path = "/empl/payment")
     public ResponseEntity<?> get(@AuthenticationPrincipal UserDetails userDetails) {
+        System.out.println(userDetails);
         Account account = accountService.get(userDetails.getUsername());
         return account == null ?
                 new ResponseEntity<>(HttpStatus.UNAUTHORIZED) :
